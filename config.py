8859cdbd2c132fb26864e4675ffab93fe12e9c78
pattoM <mumo1882@gmail.com>
2020-10-22 22:10:11 -0700

    Updating limits - 11 points to win a round, 5 games in a match

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config:
     SECRET_KEY = 'jlkjksfksdfsdsdkfkfjsksfsk' 
     SQLALCHEMY_DATABASE_URI = 'postgres://qjckgznj:owBNo4g_2Dtmdi5hrGYqcP_72kOTzO9R@lallah.db.elephantsql.com:5432/qjckgznj' #move to env in prod
-    MAX_ROUNDS = 2
-    LIM = 2
+    MAX_ROUNDS = 5
+    LIM = 10
     
 class DevelopmentConfig(Config):
     DEBUG = True 
